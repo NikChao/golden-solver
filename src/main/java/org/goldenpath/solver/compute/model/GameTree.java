0352e092f4c77523034db5a3f56587a7c2b2a0fd
Nikolai Chaourov <nik.chaourov@gmail.com>
2024-10-20 20:10:47 +1000

    game tree with test

--- src/main/java/org/goldenpath/solver/compute/model/GameTree.java
+++ src/main/java/org/goldenpath/solver/compute/model/GameTree.java
@@ -1,8 +1,11 @@
 package org.goldenpath.solver.compute.model;
 
-import java.util.ArrayList;
-import java.util.Map;
+import java.util.*;
 
+/**
+ * Game tree describing all the checks, bets, calls, raises and folds that can happen in a game
+ * TODO: support custom turn and river cards
+ */
 public class GameTree {
     public enum Street {
         PREFLOP,
@@ -31,29 +34,67 @@ public class GameTree {
             5, Street.RIVER
     );
 
-    public final double pot;
-    public final double betToMatch = 0;
+    public double pot;
+    public double betToMatch = 0;
     public int playerToAct = 0;
-    public final Action action;
+    public Action action;
     public GameTree[] children;
     public String[] board;
     public Street street = Street.PREFLOP;
+    public int lastPlayerToAct;
 
     public GameTree(CrmInput input) {
         this.action = new Action(ActionType.START, 0, null);
         this.pot = input.pot();
         this.board = input.boardCards();
         this.street = STREET_MAP.getOrDefault(board.length, Street.PREFLOP);
+        this.lastPlayerToAct = input.players().length - 1;
 
         var playerActions = getActions(input);
 
-        children = playerActions.stream().map(action -> new GameTree(input, this, action)).toArray(GameTree[]::new);
+        children = playerActions.stream().map(action -> new GameTree(
+                toNewInput(input, action),
+                nextPlayerToAct(input),
+                getLastPlayerToAct(input, action),
+                this,
+                action)).toArray(GameTree[]::new);
     }
 
-    public GameTree(CrmInput input, GameTree parent, Action action) {
+    public GameTree(CrmInput input, int playerToAct, int lastPlayerToAct, GameTree parent, Action action) {
         this.action = action;
-        this.pot = parent.pot + action.amount;
+        this.pot = input.pot();
         this.street = toStreet(parent, action);
+        this.betToMatch = action.amount;
+        this.playerToAct = playerToAct;
+        this.lastPlayerToAct = lastPlayerToAct;
+
+        if (input.players()[playerToAct].stack() <= 0) return;
+
+        if (parent.lastPlayerToAct == parent.playerToAct && List.of(ActionType.CHECK, ActionType.CALL).contains(action.type)) {
+            if (!street.RIVER.equals(street)) {
+                children = new GameTree[]{new GameTree(input, this, "2h")};
+            }
+            return;
+        }
+        if (List.of(ActionType.FOLD).contains(action.type)) {
+            return;
+        }
+
+        var playerActions = getActions(input);
+        children = playerActions.stream().map(a -> {
+            return new GameTree(toNewInput(input, a), getLastPlayerToAct(input, a), nextPlayerToAct(input), this, a);
+        }).toArray(GameTree[]::new);
+    }
+
+    public GameTree(CrmInput input, GameTree parent, String card) {
+        this.action = new Action(ActionType.CARD, 0, card);
+        this.street = parent.nextStreet();
+
+        var nextInput = toNewInput(input, action);
+
+        children = new GameTree[]{
+                new GameTree(nextInput)
+        };
     }
 
     private ArrayList<Action> getActions(CrmInput input) {
@@ -62,16 +103,31 @@ public class GameTree {
 
         if (betToMatch > 0) {
             playerActions.add(new Action(ActionType.FOLD, 0, null));
+
+            // TODO: support all in threshold
             playerActions.add(new Action(ActionType.CALL, betToMatch, null));
-            playerActions.add(new Action(ActionType.RAISE, betToMatch * 2, null));
+
+            var hasAllInned = false;
+            for (var raiseSize : getRaiseSizes(player, street)) {
+                var absoluteBet = Math.round(raiseSize * pot);
+                var allinThreshold = player.stack() * input.allinThreshold();
+                if (absoluteBet >= allinThreshold) {
+                    playerActions.add(new Action(ActionType.RAISE, player.stack(), null));
+                    hasAllInned = true;
+                } else {
+                    playerActions.add(new Action(ActionType.RAISE, absoluteBet, null));
+                }
+            }
         } else {
             playerActions.add(new Action(ActionType.CHECK, 0, null));
 
+            var hasAllInned = false;
             for (var betSize : getBetSizes(player, street)) {
                 var absoluteBet = Math.round(betSize * pot);
                 var allinThreshold = player.stack() * input.allinThreshold();
-                if (absoluteBet >= allinThreshold) {
+                if (absoluteBet >= allinThreshold && !hasAllInned) {
                     playerActions.add(new Action(ActionType.BET, player.stack(), null));
+                    hasAllInned = true;
                 } else {
                     playerActions.add(new Action(ActionType.BET, absoluteBet, null));
                 }
@@ -91,6 +147,88 @@ public class GameTree {
             case PREFLOP:
             default:
                 return new double[]{};
+        }
+    }
+
+    private double[] getRaiseSizes(PlayerParams player, Street street) {
+        switch (street) {
+            case FLOP:
+                return player.flopRaiseSizes();
+            case TURN:
+                return player.turnRaiseSizes();
+            case RIVER:
+                return player.riverRaiseSizes();
+            case PREFLOP:
+            default:
+                return new double[]{};
+        }
+    }
+
+    private int nextPlayerToAct(CrmInput input) {
+        if (playerToAct == input.players().length - 1) {
+            return 0;
+        }
+        return playerToAct + 1;
+    }
+
+    private CrmInput toNewInput(CrmInput input, Action action) {
+        var updatedPlayers = new PlayerParams[input.players().length];
+        for (int i = 0; i < input.players().length; i++) {
+            if (i == playerToAct) {
+                var playerToUpdate = input.players()[i];
+                var updatedPlayer = new PlayerParams(
+                        playerToUpdate.range(),
+                        (int) (playerToUpdate.stack() - action.amount),
+                        playerToUpdate.flopBetSizes(),
+                        playerToUpdate.flopRaiseSizes(),
+                        playerToUpdate.turnBetSizes(),
+                        playerToUpdate.turnRaiseSizes(),
+                        playerToUpdate.riverBetSizes(),
+                        playerToUpdate.riverRaiseSizes()
+                );
+                updatedPlayers[i] = updatedPlayer;
+            } else {
+                updatedPlayers[i] = input.players()[i];
+            }
+        }
+
+        var boardList = new ArrayList<String>();
+        Collections.addAll(boardList, input.boardCards());
+        if (action.card != null) {
+            boardList.add(action.card);
+        }
+
+        return new CrmInput(
+                updatedPlayers,
+                input.raiseLimit(),
+                (int) (input.pot() + action.amount),
+                input.effectiveStack(),
+                input.allinThreshold(),
+                boardList.toArray(new String[0])
+        );
+    }
+
+    private int getLastPlayerToAct(CrmInput input, Action action) {
+        if (action.type == ActionType.RAISE) {
+            if (playerToAct == 0) {
+                return input.players().length - 1;
+            }
+
+            return playerToAct - 1;
+        }
+
+        return lastPlayerToAct;
+    }
+
+    private Street nextStreet() {
+        switch (street) {
+            case PREFLOP:
+                return Street.FLOP;
+            case FLOP:
+                return Street.TURN;
+            case TURN:
+            default:
+                return Street.RIVER;
         }
     }
 
--- src/test/java/org/goldenpath/solver/compute/model/GameTreeTest.java
+++ src/test/java/org/goldenpath/solver/compute/model/GameTreeTest.java
@@ -2,8 +2,7 @@ package org.goldenpath.solver.compute.model;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GameTreeTest {
     private static final String TIGHT_RANGE_TEXT = "AhAd,AhAc,AhAs,AdAc,AdAs,AcAs,KhKd,KhKc,KhKs,KdKc,KdKs,KcKs," +
@@ -15,7 +14,7 @@ public class GameTreeTest {
     private static final String[] TIGHT_RANGE = TIGHT_RANGE_TEXT.split(",");
 
     @Test
-    public void testGameTree() {
+    public void testGameTreeCanCheckThrough() {
         var oop = new PlayerParams(TIGHT_RANGE, 200, new double[]{0.3, 0.6}, new double[]{0.5, 1.5}, new double[]{0.3, 0.6}, new double[]{0.5, 1.5}, new double[]{0.3, 0.6}, new double[]{0.5, 1.5});
         var ip = new PlayerParams(TIGHT_RANGE, 200, new double[]{0.3, 0.6}, new double[]{0.5, 1.5}, new double[]{0.3, 0.6}, new double[]{0.5, 1.5}, new double[]{0.3, 0.6}, new double[]{0.5, 1.5});
         var players = new PlayerParams[]{oop, ip};
@@ -28,5 +27,38 @@ public class GameTreeTest {
         assertEquals(gameTree.action.type(), GameTree.ActionType.START);
         assertEquals(gameTree.street, GameTree.Street.FLOP);
         assertEquals(gameTree.children.length, 3);
+
+        var oopCheckFlop = gameTree.children[0];
+        assertEquals(GameTree.ActionType.CHECK, oopCheckFlop.action.type());
+
+        var ipCheckFlop = oopCheckFlop.children[0];
+        assertEquals(GameTree.ActionType.CHECK, ipCheckFlop.action.type());
+
+        var turnCard = ipCheckFlop.children[0];
+        assertEquals(GameTree.ActionType.CARD, turnCard.action.type());
+
+        var startTurn = turnCard.children[0];
+        assertEquals(GameTree.ActionType.START, startTurn.action.type());
+
+        var oopCheckTurn = startTurn.children[0];
+        assertEquals(GameTree.ActionType.CHECK, oopCheckTurn.action.type());
+
+        var ipCheckTurn = oopCheckTurn.children[0];
+        assertEquals(GameTree.ActionType.CHECK, ipCheckTurn.action.type());
+
+        var river = ipCheckTurn.children[0];
+        assertEquals(GameTree.ActionType.CARD, river.action.type());
+
+        var startRiver = river.children[0];
+        assertEquals(GameTree.ActionType.START, startRiver.action.type());
+
+        var oopCheckRiver = startRiver.children[0];
+        assertEquals(GameTree.ActionType.CHECK, oopCheckRiver.action.type());
+
+        var ipCheckRiver = oopCheckRiver.children[0];
+        assertEquals(GameTree.ActionType.CHECK, ipCheckRiver.action.type());
+
+        // After checking back the river there should be no more game to play
+        assertNull(ipCheckRiver.children);
     }
 }
